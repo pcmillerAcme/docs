982881edaf8adea3ebdcb76c24c7cfa5c89529d8
pcmillerAcme <peter@acme.codes>
2015-10-11 15:36:02 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -123,7 +123,7 @@ html_theme = 'alabaster'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'Acme API'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -132,7 +132,7 @@ html_theme = 'alabaster'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'http://service.acme.codes/favicon'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -175,7 +175,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
@@ -268,7 +268,7 @@ man_pages = [
 texinfo_documents = [
   (master_doc, 'AcmeCodes', u'Acme.Codes Documentation',
    author, 'AcmeCodes', 'How to use service.acme.codes to generate animated codes.',
-   'Qr Codes Animation'),
+   'APIs'),
 ]
 
 # Documents to append as an appendix to all manuals.
